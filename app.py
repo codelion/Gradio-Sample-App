6f81fd8cea31e1e3451146c6c40d1d2612a9f194
IshanChakraborty96 <ishan.chakraborty@katonic.ai>
2023-06-08 18:08:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,12 @@
-import numpy as np
 import gradio as gr
 
-
-def flip_text(x):
-    return x[::-1]
-
-
-def flip_image(x):
-    return np.fliplr(x)
-
+def greet(name):
+    return "Hello " + name + "!"
 
 with gr.Blocks() as demo:
-    gr.Markdown("Flip text or image files using this demo.")
-    with gr.Tab("Flip Text"):
-        text_input = gr.Textbox()
-        text_output = gr.Textbox()
-        text_button = gr.Button("Flip")
-    with gr.Tab("Flip Image"):
-        with gr.Row():
-            image_input = gr.Image()
-            image_output = gr.Image()
-        image_button = gr.Button("Flip")
-
-    with gr.Accordion("Open for More!"):
-        gr.Markdown("Look at me...")
-
-    text_button.click(flip_text, inputs=text_input, outputs=text_output)
-    image_button.click(flip_image, inputs=image_input, outputs=image_output)
+    name = gr.Textbox(label="Name")
+    output = gr.Textbox(label="Output Box")
+    greet_btn = gr.Button("Greet")
+    greet_btn.click(fn=greet, inputs=name, outputs=output, api_name="greet")
 
 demo.launch(share=False, debug=True, server_port=8050, server_name="0.0.0.0")
